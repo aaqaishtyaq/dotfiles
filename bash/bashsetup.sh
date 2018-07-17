313ba6d356975466677b4623e168b355e1441957
aaqaishtyaq <aaqaishtyaq@gmail.com>
2018-07-17 16:09:12 +0530

    :wrench: [WIP] Added bash setup script

---
+++ bash/bashsetup.sh
@@ -0,0 +1,17 @@
+#Install
+sudo -v
+install_util_linux() {
+    platform=$(uname);
+            if [[ $platform == 'Linux' ]]; then
+                if [[ -f /etc/redhat-release ]]; then
+                    sudo yum -y update
+                    sudo yum -y upgrade
+                    sudo yum -y install fortune cowsay tmux git
+                fi
+                elif [[ -f /etc/debian_version ]]; then
+                    sudo apt-get update
+                    sudo apt-get upgrade
+                    sudo apt-get install fortune cowsay tmux git
+                fi
+        fi
+}
