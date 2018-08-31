e3da6dcc02d4053afc2389ee260fde57b4abcefd
aaqaishtyaq <aaqaishtyaq@gmail.com>
2018-08-31 18:55:46 +0530

    tweak server setup

--- server/setup.sh
+++ server/setup.sh
@@ -4,11 +4,11 @@ echo "Beginning with Debian setup. Have a walk while the machine is setup"
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install git neofetch fortune cowsay wget curl htop
+sudo apt-get -y install git neofetch fortune cowsay wget curl htop tmux
 sudo apt-get -y install python3-pip
 sudo apt-get -y install zsh
 sudo pip3 install powerline-status
-sudo apt-get install fonts-powerline
+sudo apt-get -y install fonts-powerline
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s "$(which zsh)"
 # Zsh Syntax highlighting
@@ -17,12 +17,10 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 tmuxconf() {
-    mkdir tmux
-    cd tmux
+    cd
     git clone https://github.com/gpakosz/.tmux.git
     ln -s -f .tmux/.tmux.conf
     cp .tmux/.tmux.conf.local .
 }
 
-tmuxconf
 exit 0
