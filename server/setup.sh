bdd90adce464d03e520a81bdbc72bf08ca42c16a
Aaqa Ishtyaq <aaqaishtyaq@gmail.com>
2019-04-08 10:13:20 +0530

    Update setup.sh

--- server/setup.sh
+++ server/setup.sh
@@ -2,6 +2,13 @@
 
 echo "Beginning with Debian setup. Have a walk while the machine is setup"
 
+tmuxconf() {
+    cd
+    git clone https://github.com/gpakosz/.tmux.git
+    ln -s -f .tmux/.tmux.conf
+    cp .tmux/.tmux.conf.local .
+}
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install git neofetch fortune cowsay wget curl htop tmux dnsutils screen
@@ -14,16 +21,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 # Zsh Syntax highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
+tmuxconf()
 # Finally Change the Shell :-)
 sudo chsh -s "$(which zsh)"
-
-tmuxconf() {
-    cd
-    git clone https://github.com/gpakosz/.tmux.git
-    ln -s -f .tmux/.tmux.conf
-    cp .tmux/.tmux.conf.local .
-}
 
 
 
