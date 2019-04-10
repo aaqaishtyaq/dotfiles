d20464cc932c17aba4a33f028e46e0f6dc08c4cb
Aaqa Ishtyaq <aaqaishtyaq@gmail.com>
2019-04-10 15:41:12 +0530

    Update setup.sh

--- server/setup.sh
+++ server/setup.sh
@@ -14,13 +14,13 @@ sudo apt-get -y upgrade
 sudo apt-get -y install git neofetch fortune cowsay wget curl htop tmux dnsutils screen
 sudo apt-get -y install python3-pip
 sudo apt-get -y install zsh
-cp .zshrc ~/
-cp .vimrc ~/
-cp .screenrc ~/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # Zsh Syntax highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+yes | cp -rf .zshrc ~/
+cp .vimrc ~/
+cp .screenrc ~/
 tmuxconf()
 # Finally Change the Shell :-)
 sudo chsh -s "$(which zsh)"
