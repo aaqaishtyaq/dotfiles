19c387d8d6facd5de18fceb401b93328427c9aca
Aaqa Ishtyaq <aaqa@hackerrank.com>
2020-07-30 14:03:22 +0530

    Makefile: add Makefile, update shellcheck
    Signed-off-by: Aaqa Ishtyaq <aaqa@hackerrank.com>

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
-###############################################################################
-# Setting Up Mac                                                              #
-###############################################################################
+# setup - system setup script for macOS
+# =========================================================
+# - https://github.com/aaqaishtyaq/dotfiles
+# - https://aaqa.dev
 
 echo "Running Mac setup. This would take a while. Please sit back and relax."
 
@@ -12,6 +13,9 @@ then
   echo "Installing Homebrew for you."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+# SSH Keygen, Add to ssh-agent and icloud Keychain
+cp ssh/config ~/.ssh/
 
 tmux_28 () {
     brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/834d93c87ea0f6c0c10295599b6b1fc7b4aded96/Formula/tmux.rb
@@ -29,21 +33,6 @@ install_oh_my_zsh () {
     fi
 }
 
-link_config () {
-    cp -f "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak" 2>/dev/null || true
-    cp -a ./tmux/. "$HOME"/.tmux/
-    ln -sf .tmux/tmux.conf "$HOME"/.tmux.conf;
-
-    mkdir -p ~/.config/nvim ~/.config/zsh ~/.config/vim ~/.config/tmux
-    cp -a alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-    cp -a zsh/.zshrc ~/.config/zsh/zshrc
-    cp -a vim/.vimrc ~/.config/vim/vimrc
-    cp -a tmux/next/.tmux.conf ~/.config/tmux/tmux.conf
-    ls -sf ~/.config/zsh/zshrc "$HOME"/.zshrc;
-    ls -sf ~/.config/vim/vimrc "$HOME"/.vimrc;
-    ls -sf ~/.config/tmux/tmux.conf "$HOME"/.tmux.conf;
-}
-
 setup_ohmyzsh () {
     git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
     git clone https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
@@ -59,15 +48,12 @@ install_nvm () {
     curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 }
 
-# SSH Keygen, Add to ssh-agent and icloud Keychain
-cp ssh/config ~/.ssh/
-
 brew update
 brew upgrade
 brew bundle
 brew cleanup && brew cask cleanup
 install_oh_my_zsh
-link_config
+setup_ohmyzsh
 install_nvm
 "$(brew --prefix)/opt/fzf/install"
 
--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# tests - Shellcheck based tests
+# =========================================================
+# - https://github.com/aaqaishtyaq/dotfiles
+# - https://aaqa.dev
 
 set -e
 set -o pipefail
@@ -8,12 +13,18 @@ RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 RESET=$(tput sgr0)
 
+# SHELLCHECK Ignore
+# SC2034 = foo appears unused. Verify it or export it. "For Powerline Theme"
+# SC1090 = Can't follow non-constant source. Use a directive to specify location.
+# SC2015 = Note that A && B || C is not if-then-else. C may run when A is true.
+# SC2046 = Quote this to prevent word splitting
+export SHELLCHECK_OPTS="-e SC2034 -e SC1090 -e SC2015 -e SC2046"
 
 # find all executables and run `shellcheck`
 for f in $(find . -type f -not -iwholename '*.git*' | sort -u); do
     if file "$f" | grep --quiet shell; then
         {
-            shellcheck "$f" && echo "${GREEN}[OK]: sucessfully linted $f ${RESET}"
+            shellcheck -x "$f" && echo "${GREEN}[OK]: sucessfully linted $f ${RESET}"
         } || {
             # add to errors
             ERRORS+=("$f")
