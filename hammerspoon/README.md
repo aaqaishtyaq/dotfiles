24383c7f62fec9a309c24059411c438279ceaca0
Aaqa Ishtyaq <aaqa@hackerrank.com>
2021-04-09 12:35:53 +0530

    hammerspoon: add config
    Signed-off-by: Aaqa Ishtyaq <aaqa@hackerrank.com>

---
+++ hammerspoon/README.md
@@ -0,0 +1,7 @@
+# Hammerspoon
+
+To set your hammerspoon config to look in $XDG_CONFIG_HOME Run
+
+```sh
+defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
+```
---
+++ hammerspoon/init.lua
@@ -0,0 +1,37 @@
+-- Hammerspoon modifier
+local hsModifier = { "ctrl", "alt" }
+
+local appList = {
+	Alacritty = { binding = "a"},
+	Slack = { binding = "s" },
+	Firefox = { binding = "f" },
+	-- ["Code"] = { binding = "e" }
+}
+
+-- Reload config
+hs.hotkey.bind(hsModifier, "R", function()
+	hs.reload()
+	-- hs.notify.new({title="Hammerspoon", informativeText="Config loaded"}):send()
+	hs.alert.show('Config Reloaded')
+end)
+
+alertId = nil
+hs.application.enableSpotlightForNameSearches(true)
+
+-- Attach app keybindings
+for app, config in pairs(appList) do
+	-- Bind the app
+	if config.binding ~= nil then
+		hs.hotkey.bind(hsModifier, config.binding, function()
+			application = hs.application.find(app)
+			if config.autoHide then
+				-- Noop
+			elseif application then
+				hs.application.launchOrFocus(app)
+			else
+				hs.alert.closeSpecific(alertID, 0)
+				alertID = hs.alert(app .. " not Running!", nil, nil, 1)
+			end
+		end)
+	end
+end
