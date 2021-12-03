63dbbeb161f5718d756e29e22b5e637a1134a96b
Aaqa Ishtyaq <aaqa@hackerrank.com>
2021-12-03 13:45:40 +0530

    config/*,bin/*: update hms, term and git-exclude
      * add bin to exlude file in git diff.
      * add hammerspoon config to move windows between monitors.
      * update alacritty config to remove window decoration
      * update alacritty config to change color scheme to gruvbox dark
    
    Signed-off-by: Aaqa Ishtyaq <aaqa@hackerrank.com>

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -37,3 +37,15 @@ for app, config in pairs(appList) do
     end)
   end
 end
+
+-- bind hotkey
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'n', function()
+  -- get the focused window
+  local win = hs.window.focusedWindow()
+  -- get the screen where the focused window is displayed, a.k.a. current screen
+  local screen = win:screen()
+  -- compute the unitRect of the focused window relative to the current screen
+  -- and move the window to the next screen setting the same unitRect
+  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+end)
+
